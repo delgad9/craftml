d0f9f284017ce5c143979d73dcb1a8f018e0f3da
Tom Yeh <doubleshow@gmail.com>
2015-02-08 20:28:32 -0700

    basic implementation

--- test/render.test.js
+++ test/render.test.js
@@ -1,11 +1,14 @@
 var chai = require('chai'),
+    assert = require('chai').assert,
     sinon = require("sinon"),
-    sinonChai = require("sinon-chai")
+    sinonChai = require("sinon-chai"),
+    debug = require('debug')('craftml.test.render')
 
 chai.should()
 chai.use(sinonChai);
 
 var render = require('../lib/render'),
+    Solid = require('../lib/solid1'),
     chaiSubset = require('chai-subset'),
     inspect = require('eyes').inspector(),
     EventEmitter = require("events").EventEmitter
@@ -20,7 +23,15 @@ make(f, 'content')
 make(f, 'script')
 make(f, 'u1')
 make(f, 'u2')
+make(f, 'csg')
 make(f, 'foo')
+make(f, 'factory')
+make(f, 'unit')
+make(f, 'group')
+
+makea('p1')
+makea('def', 'default')
+makea('type')
 
 // function MockScript() {
 //     this.name = "script"
@@ -38,9 +49,45 @@ make(f, 'foo')
 
 function id(id) {
     return {
-        id: id
+        a: {
+            id: id
+        }
     }
 }
+
+function name(name) {
+    return {
+        a: {
+            name: name
+        }
+    }
+}
+
+function a(name, value) {
+    var o = {}
+    o[name] = value
+    return {
+        a: o
+    }
+}
+
+function makea(name, as) {
+
+    var f = function(val) {
+        var o = {}
+        if (as)
+            o[as] = val
+        else
+            o[name] = val
+        return {
+            a: o
+        }
+    }
+
+
+    eval(name + ' = f')
+}
+
 
 function make(f, name) {
 
@@ -48,32 +95,39 @@ function make(f, name) {
         var args = Array.prototype.slice.call(arguments)
 
         var el = {
+            type: 'tag',
             name: name,
-            attribs: []
+            attribs: {}
         }
 
         var children = []
 
         args.forEach(function(arg) {
 
-            if ('id' in arg) {
-                el.attribs.id = arg.id
+            if ('a' in arg) {
+                _.extend(el.attribs, arg.a)
+
             } else {
 
                 children.push(arg)
             }
 
+            // console.log('arg',arg)
+
         })
 
-        if (name === 'script') {
+        // console.log(el.attribs)
 
-            el.type = 'text/openjscad'
-            el.eval = function() {
-                el.layout = {
+        if (name === 'script' || name === 'factory' || name === 'unit') {
+            el.type = 'factory'
+            // el.attribs.type = 'text/openjscad'
+            el.create = function() {
+                var s = new Solid()
+                s.layout = {
                     size: {
-                        x: 5,
-                        y: 5,
-                        z: 5
+                        x: 1,
+                        y: 1,
+                        z: 1
                     },
                     location: {
                         x: 0,
@@ -81,6 +135,7 @@ function make(f, name) {
                         z: 0
                     }
                 }
+                return s
             }
             delete el.children
 
@@ -96,50 +151,201 @@ function make(f, name) {
 }
 
 
-describe('NEW render', function() {
-
-    describe('mock script', function() {
-
-        it('a script is evaluated once', function() {
-
-            var spy = sinon.spy()
-            var c = script()
-
-            c.eval = spy
-            render(c)
-
-            spy.should.have.been.calledOnce
-        })
+function solid() {
+    return {
+        type: 'solid'
+    }
+}
 
 
-        it('a craft\'s children scripts are both evaluated', function() {
+function grp() {
+    var args = Array.prototype.slice.call(arguments)
 
-            var c = craft(script(), script())
+    return {
+        type: 'group',
+        children: args
+    }
+}
 
-            c.children[0].eval = sinon.spy()
-            c.children[1].eval = sinon.spy()
+function match(actual, expected) {
 
-            render(c)
-            
-            c.children[0].eval.should.have.been.calledOnce
-            c.children[1].eval.should.have.been.calledOnce
 
-        })
+    if (_.isArray(expected) && _.isArray(actual)) {
 
-        it('an array of scripts are all evaluated', function() {
+        actual.should.have.length(expected.length)
 
-            var c = [script(), script()]
+        for (var i = 0; i < actual.length; i++) {
 
-            c[0].eval = sinon.spy()
-            c[1].eval = sinon.spy()
+            actual[i].type.should.be.equal(expected[i].type)
 
-            render(c)
-            
-            c[0].eval.should.have.been.calledOnce
-            c[1].eval.should.have.been.calledOnce
+        }
 
-        })
+    } else {
+
+        actual.type.should.be.equal(expected.type)
+
+        if (actual.children)
+            match(actual.children, expected.children)
+
+    }
+
+}
+
+describe('render#', function() {
+
+    it('can render a single unit', function() {
+
+        var c = unit()
+
+        var r = render(c)
+
+        match(r, solid())
+    })
+
+    it('can render an array of two units', function() {
+
+        var c = [unit(), unit()]
+
+        var r = render(c)
+
+        match(r, [solid(), solid()])
+    })
+
+    it('can render a group of two units', function() {
+
+        var c = group(unit(), unit())
+
+        var r = render(c)
+
+        match(r,
+            grp(solid(),
+                solid()))
+    })
+
+    it('can render a group of two units within another group', function() {
+
+        var c = group(unit(), group(unit(), unit()))
+            // inspect(c)
+
+        var r = render(c)
+            // inspect(r)
+
+        match(r,
+            grp(solid(),
+                grp(solid(),
+                    solid())))
+    })
+
+    it('does not render <craft> to anything', function() {
+
+        var c = craft(unit())
+            // inspect(c)
+
+        var r = render(c)
+            // inspect(r)
+
+        assert.isUndefined(r)
 
     })
+
+    it('can resolve <foo> to <craft name="foo"><unit>', function() {
+
+        var c = [
+            craft(a('name', 'foo'),
+                unit()),
+            foo()
+        ]
+
+        // inspect(c)
+
+        var r = render(c)
+            // inspect(r)
+
+        match(r, [solid()])
+
+    })
+
+    it('can resolve <foo> three times', function() {
+
+        var c = [
+            craft(a('name', 'foo'),
+                unit()),
+            foo(),
+            foo(),
+            foo()
+        ]
+
+        // inspect(c)
+
+        var r = render(c)
+        // inspect(r)
+
+        match(r, [solid(), solid(), solid()])
+
+    })
+
+    // describe('mock script', function() {
+
+    //     it('a script is evaluated once', function() {
+
+    //         var spy = sinon.spy()
+    //         var c = script()
+
+    //         c.eval = spy
+    //         render(c)
+
+    //         spy.should.have.been.calledOnce
+    //     })
+
+
+    //     it('a craft\'s children scripts are both evaluated', function() {
+
+    //         var c = craft(script(), script())
+
+    //         c.children[0].eval = sinon.spy()
+    //         c.children[1].eval = sinon.spy()
+
+    //         render(c)
+
+    //         c.children[0].eval.should.have.been.calledOnce
+    //         c.children[1].eval.should.have.been.calledOnce
+
+    //     })
+
+    //     it('an array of scripts are all evaluated', function() {
+
+    //         var c = [script(), script()]
+
+    //         c[0].eval = sinon.spy()
+    //         c[1].eval = sinon.spy()
+
+    //         render(c)
+
+    //         c[0].eval.should.have.been.calledOnce
+    //         c[1].eval.should.have.been.calledOnce
+
+    //     })
+
+    //     it.only('an embedded script', function() {
+
+    //         var s = script()
+
+    //         var c = [
+    //             craft(name('foo'),
+    //                 parameter(a('name','p1'), a('default',3), a('type','int')),
+    //                 script()),
+    //             foo(p1('a')),
+    //             foo(p1('b'))
+    //         ]
+
+    //         var d = [csg(),csg()]
+    //         inspect(c)
+    //         render(c)
+    //         inspect(d)
+
+    //     })
+
+    // })
+
 
 })
