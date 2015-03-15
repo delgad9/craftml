d73bf7f7a3f89be78fefab36a750e9a567a9733f
Tom Yeh <doubleshow@gmail.com>
2015-03-15 15:38:13 -0600

    browserify known to work

--- browserify.js
+++ browserify.js
@@ -1,7 +1,7 @@
-var b = require('browserify')(),
+var b = require('browserify')({debug:true}),
     fs = require('fs'),
     p = require('partialify/custom');
  
 b.add('./lib/browser.js');
 b.transform(p.alsoAllow('xml'));
-b.bundle().pipe(fs.createWriteStream('./editor/public/bundle.js'));
+b.bundle().pipe(fs.createWriteStream('../craftml-docs/contents/bundle.js'));
