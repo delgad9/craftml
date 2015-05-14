51e2f644b323e5b8230374eca484227831c1dc26
Tom Yeh <doubleshow@gmail.com>
2015-05-13 22:45:31 -0600

    refactored csg shapes to individual files

--- test/csg.test.js
+++ test/csg.test.js
@@ -8,17 +8,78 @@ var chai = require('chai'),
 var CSG1 = require('../lib/scad/csg'),
 CSG2 = require('craft-scad1').CSG
 
+
+function test(f){
+    var c1 = f(CSG1)
+    var c2 = f(CSG2)
+    c1.toString().length.should.be.eql(c2.toString().length)
+    c1.toString().slice(0,100).should.be.eql(c2.toString().slice(0,100))
+}
+
 describe('#csg', function() {
 
     it('shapes', function() {
 
-        var c1 = new CSG1.cube().translate([1,1,1])
-        var c2 = new CSG2.cube().translate([1,1,1])
-        // inspect(c1)
-        // inspect(c2)
-        // console.log(c1.toString())
-        // console.log(c2.toString())
-        c1.toString().should.be.eql(c2.toString())
+        test(function(CSG){
+            return new CSG.cube()
+        })
+
+        test(function(CSG){
+            console.log(CSG)
+            return new CSG.sphere()
+        })
+
+        test(function(CSG){
+            return new CSG.cylinder()
+        })
+
+        test(function(CSG){
+            return new CSG.roundedCylinder()
+        })
+
+        test(function(CSG){
+            return new CSG.roundedCube()
+        })
+
+    })
+
+    it('transform', function() {
+
+        test(function(CSG){
+            return new CSG.cube().translate([1,1,1])
+        })
+
+        test(function(CSG){
+            return new CSG.cube().scale([2,2,2])
+        })
+
+        test(function(CSG){
+            return new CSG.cube().rotateX(45)
+        })
+
+        test(function(CSG){
+            return new CSG.cube().rotateY(45)
+        })
+
+        test(function(CSG){
+            return new CSG.cube().rotateZ(45)
+        })
+
+    })
+
+    it('set operations', function() {
+
+        test(function(CSG){
+            return new CSG.cube().translate([1,1,1]).union(new CSG.cube())
+        })
+
+        test(function(CSG){
+            return new CSG.cube().translate([0.5,0.5,0.5]).subtract(new CSG.cube())
+        })
+
+        test(function(CSG){
+            return new CSG.cube().translate([0.5,0.5,0.5]).intersect(new CSG.cube())
+        })
 
     })
 })
