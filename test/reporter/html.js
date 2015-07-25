6a52c7160b2c68fee6dcb497146a49398e1520b6
Tom Yeh <doubleshow@gmail.com>
2015-07-25 05:43:30 -0600

    selective layout

--- test/reporter/html.js
+++ test/reporter/html.js
@@ -1,9 +1,11 @@
 var jade = require('jade')
 var _ = require('lodash')
 
+var modelsPerPage = 9
+
 module.exports = function(titles){
 
-    var pages = _.map(_.chunk(titles, 4), function(ch, i){
+    var pages = _.map(_.chunk(titles, modelsPerPage), function(ch, i){
 
         var items = _.map(ch, function(m){
 
